10564f47bc76b1b2d6815190c90a61f7a13da461
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-08-17 14:49:19 +0700

    refactor: remove automatic website_id assignment and website relationship from Media model

--- api/app/Models/Media.php
+++ api/app/Models/Media.php
@@ -9,24 +9,4 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia;
 class Media extends BaseMedia
 {
     use HasUuids;
-
-    protected static function booted(): void
-    {
-        parent::booted();
-
-        static::creating(function (Media $media) {
-            if (empty($media->website_id) && $media->model) {
-                if ($media->model instanceof Website) {
-                    $media->website_id = $media->model->id;
-                } elseif (isset($media->model->website_id)) {
-                    $media->website_id = $media->model->website_id;
-                }
-            }
-        });
-    }
-
-    public function website(): BelongsTo
-    {
-        return $this->belongsTo(Website::class);
-    }
 }
