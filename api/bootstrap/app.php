067868f34fa86b4120e6ae355ba43c2f8327cb51
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-08-17 16:15:52 +0700

    chore: disable web routes and force JSON error responses in application bootstrap

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -4,10 +4,11 @@ use App\Http\Middleware\EnsureVendorMember;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        // web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
@@ -19,5 +20,13 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            // if ($request->is('api/*')) {
+            //     return true;
+            // }
+
+            // return $request->expectsJson();
+
+            return true;
+        });
     })->create();
